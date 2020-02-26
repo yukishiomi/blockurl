3134e751130c26c9f887d5e5bbfca3ad38590668
Yuki Shiomi <luckey.ss0209@gmail.com>
2020-02-26 12:04:19 +0900

    ロジックの変更

--- public/myscript.js
+++ public/myscript.js
@@ -1,14 +1,16 @@
 let array = []
 chrome.storage.local.get(["prohibition"], function(items) {
   const storage = items.prohibition
-  console.log(storage)
   let search = document.getElementsByTagName('a')
-  for (let i = 0, len = search.length; i < len; i++) {
-    storage.map((list) => {
+  storage.map((list) => {
+    for (let i = 0, len = search.length; i < len; i++) {
       if(search[i].href.toString().includes(list)){
-        console.log(search[i])
-        search[i].parentNode.parentNode.parentNode.parentNode.remove()
+        array.push(search[i])
       }
-    })
-  }
+    }
+  })
+  array.map((dom) => {
+    console.log(dom)
+    dom.parentNode.parentNode.parentNode.parentNode.remove()
+  })
 });
